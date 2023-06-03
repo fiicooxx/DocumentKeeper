5ff6da1e3952f517b84c74d902c2e10b093d25dd
Dawid Mielke <dawidmielke.tri@gmail.com>
2023-06-03 15:06:09 +0200

    changes

--- Infrastucture/Data/ApplicationDbContext.cs
+++ Infrastucture/Data/ApplicationDbContext.cs
@@ -11,15 +11,12 @@ namespace Infrastructure.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
             optionsBuilder.UseSqlServer(
                 "DATA SOURCE=localhost\\SQLEXPRESS;DATABASE=Lab6StudiaToken;Integrated Security=true;TrustServerCertificate=True");
         }
-
         public DbSet<Document> Documents { get; set; }
     }
 }
