8a47c0b9634960413699c5fdc5b94249fde27020
Filip Copija <Filip.Copija@microsoft.wsei.edu.pl>
2023-06-03 15:01:21 +0200

    add dependency injection to DocumentRepository

--- DocumentKeeper/Program.cs
+++ DocumentKeeper/Program.cs
@@ -1,7 +1,11 @@
+using Infrastructure.Repositories;
+using Infrastructure.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddScoped<IDocumentRepository, DocumentRepository>();
 
 var app = builder.Build();
 
@@ -12,6 +16,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
