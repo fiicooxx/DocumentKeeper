956d23bac9780bd569e8a621529a1fe8bc5cc55e
Filip Copija <Filip.Copija@microsoft.wsei.edu.pl>
2023-06-10 23:57:32 +0200

    sample login page + logic

---
+++ DocumentKeeper/Pages/Login.cshtml.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web.Pages
+{
+    public class LoginModel : PageModel
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public LoginModel(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            public string Username { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(Input.Username, Input.Password, false, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                return RedirectToPage("/Index");
+            }
+            else
+            {
+                ViewData["ErrorMessage"] = "Invalid login attempt.";
+                return Page();
+            }
+        }
+    }
+}
