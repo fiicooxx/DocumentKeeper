adabf6077a0a4bf66a136378de1d9d92c3bde99d
Filip Copija <Filip.Copija@microsoft.wsei.edu.pl>
2023-06-10 23:47:57 +0200

    Update registration

--- DocumentKeeper/Pages/Register.cshtml.cs
+++ DocumentKeeper/Pages/Register.cshtml.cs
@@ -1,12 +1,68 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace Web.Pages
 {
     public class RegisterModel : PageModel
     {
-        public void OnGet()
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public RegisterModel(UserManager<IdentityUser> userManager)
         {
+            _userManager = userManager;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            public string Username { get; set; }
+
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var user = new IdentityUser
+            {
+                UserName = Input.Username,
+                Email = Input.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, Input.Password);
+            if (result.Succeeded)
+            {
+                //TODO: LoginPage
+                return RedirectToPage("Login");
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+                return Page();
+            }
         }
     }
 }
