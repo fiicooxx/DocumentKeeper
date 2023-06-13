dbd119a503c38d690000f8b52c13ea7f2579d8a4
fiicooxx <filip.copija1@gmail.com>
2023-06-13 11:51:26 +0200

    Auth Controllers web api

---
+++ WebApi/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+[Route("api/login")]
+public class LoginController : ControllerBase
+{
+    private readonly SignInManager<IdentityUser> _signInManager;
+
+    public LoginController(SignInManager<IdentityUser> signInManager)
+    {
+        _signInManager = signInManager;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login([FromBody] LoginModel model)
+    {
+        var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, lockoutOnFailure: false);
+
+        if (result.Succeeded)
+        {
+            return Ok();
+        }
+
+        return Unauthorized();
+    }
+}
+
+public class LoginModel
+{
+    [Required(ErrorMessage = "Email is required.")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Password is required.")]
+    public string Password { get; set; }
+}
---
+++ WebApi/Controllers/RegisterController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+[Route("api/register")]
+public class RegisterController : ControllerBase
+{
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+
+    public RegisterController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register([FromBody] RegisterModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var user = new IdentityUser
+        {
+            UserName = model.Email,
+            Email = model.Email
+        };
+
+        var result = await _userManager.CreateAsync(user, model.Password);
+
+        if (result.Succeeded)
+        {
+            if (!await _roleManager.RoleExistsAsync(model.Role))
+            {
+                var role = new IdentityRole(model.Role);
+                await _roleManager.CreateAsync(role);
+            }
+
+            await _userManager.AddToRoleAsync(user, model.Role);
+
+            return Ok();
+        }
+
+        return BadRequest(result.Errors);
+    }
+}
+
+public class RegisterModel
+{
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Invalid email address.")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Password is required.")]
+    [DataType(DataType.Password)]
+    [MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
+    public string Password { get; set; }
+
+    [Required(ErrorMessage = "Confirm Password is required.")]
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "Password and Confirm Password must match.")]
+    public string ConfirmPassword { get; set; }
+
+    public string Role { get; set; }
+}
